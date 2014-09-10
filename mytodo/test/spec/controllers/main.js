12f00c2f4213da35e477e24a35a1ef8af54c2985
XTREEM <jrramsaransing@gmail.com>
2014-09-10 13:27:19 +0200

    format ffs

--- mytodo/test/spec/controllers/main.js
+++ mytodo/test/spec/controllers/main.js
@@ -2,23 +2,23 @@
 
 describe("Controller: MainCtrl", function () {
 
-  // load the controller"s module
-  beforeEach(module("mytodoApp"));
+    // load the controller"s module
+    beforeEach(module("mytodoApp"));
 
-  var MainCtrl,
-    scope;
+    var MainCtrl,
+        scope;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    MainCtrl = $controller("MainCtrl", {
-      $scope: scope
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function ($controller, $rootScope) {
+        scope = $rootScope.$new();
+        MainCtrl = $controller("MainCtrl", {
+            $scope: scope
+        });
+    }));
+
+    it("should have no items to start", function () {
+        expect(scope.todos.length).toBe(0);
     });
-  }));
-
-  it("should have no items to start", function () {
-    expect(scope.todos.length).toBe(0);
-  });
 
     it("should at 1 item to the list", function () {
         scope.todo = "Todo 1";
